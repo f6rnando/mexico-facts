ff4aeac8d233518b2d6d9461b947a940eb14a3f7
Pinto, Fernando <fernando.pinto@thermofisher.com>
2019-09-11 16:18:52 -0700

    First commit. Mexico Facts Google Chrome extension

---
+++ assets/js/entries.js
@@ -0,0 +1,27 @@
+// Author: Fernando Pinto f6rnando@gmail.com
+
+var entries = [
+    { "question": "Deidad del México antiguo conocida como la Serpiente Emplumada", "answer": "Quetzalcóatl" },
+    { "question": "Cultura que se desarrolló en la ciudad de Palenque", "answer": "Maya" },
+    { "question": "Las cabezas colosales son creaciones de la cultura", "answer": "Olmeca" },
+    { "question": "Terreno construido sobre las lagunas del valle de México que sirvió de base al sistema productivo del altiplano central. En la actualidad, puede encontrarse en Xochimilco", "answer": "Chinampa" },
+    { "question": "¿En dónde se encuentran las pirámides del Sol y la Luna?", "answer": "Teotihuacán" },
+    { "question": "Manuscrito en papel amate o sobre cuero con representaciones pictóricas que relataba asuntos históricos", "answer": "Códice" }
+];
+
+var backgrounds = [
+    "#cc5a5a",
+    "#e67349",
+    "#edb947",
+    "#c4c431",
+    "#86ba56",
+    "#6dd17c",
+    "#36e3a1",
+    "#34afbf",
+    "#5e88d1",
+    "#9b8dc2",
+    "#ad58cc",
+    "#c767bf",
+    "#c44183",
+    "#c25757"
+];
