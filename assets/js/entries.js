bea21014d1b021cede5fce8310d9b87ac8de4fa0
Pinto, Fernando <fernando.pinto@thermofisher.com>
2019-09-12 11:53:59 -0700

    More entries added

--- assets/js/entries.js
+++ assets/js/entries.js
@@ -1,12 +1,382 @@
 // Author: Fernando Pinto f6rnando@gmail.com
 
-var entries = [
-    { "question": "Deidad del México antiguo conocida como la Serpiente Emplumada", "answer": "Quetzalcóatl" },
-    { "question": "Cultura que se desarrolló en la ciudad de Palenque", "answer": "Maya" },
-    { "question": "Las cabezas colosales son creaciones de la cultura", "answer": "Olmeca" },
-    { "question": "Terreno construido sobre las lagunas del valle de México que sirvió de base al sistema productivo del altiplano central. En la actualidad, puede encontrarse en Xochimilco", "answer": "Chinampa" },
-    { "question": "¿En dónde se encuentran las pirámides del Sol y la Luna?", "answer": "Teotihuacán" },
-    { "question": "Manuscrito en papel amate o sobre cuero con representaciones pictóricas que relataba asuntos históricos", "answer": "Códice" }
+var entries = [{
+        "question": "Deidad del México antiguo conocida como la Serpiente Emplumada",
+        "answer": "Quetzalcóatl"
+    },
+    {
+        "question": "Cultura que se desarrolló en la ciudad de Palenque",
+        "answer": "Maya"
+    },
+    {
+        "question": "Las cabezas colosales son creaciones de la cultura",
+        "answer": "Olmeca"
+    },
+    {
+        "question": "Terreno construido sobre las lagunas del valle de México que sirvió de base al sistema productivo del altiplano central. En la actualidad, puede encontrarse en Xochimilco",
+        "answer": "Chinampa"
+    },
+    {
+        "question": "¿En dónde se encuentran las pirámides del Sol y la Luna?",
+        "answer": "Teotihuacán"
+    },
+    {
+        "question": "Manuscrito en papel amate o sobre cuero con representaciones pictóricas que relataba asuntos históricos",
+        "answer": "Códice"
+    },
+    {
+        "question": "Artista conocido por sus figuras geométricas",
+        "answer": "Enrique Carvajal Sebastián"
+    },
+    {
+        "question": "Se le llamaban criollo a",
+        "answer": "Hijos de Españoles nacidos en Mexico"
+    },
+    {
+        "question": "Quien fundó Villa Rica de la Vera Cruz",
+        "answer": "Hernán Cortéz"
+    },
+    {
+        "question": "Río más largo de México",
+        "answer": "Río Bravo"
+    },
+    {
+        "question": "Ciudad más poblada de México",
+        "answer": "CDMX"
+    },
+    {
+        "question": "Sitio dónde estudiaban los indígenas en el virreinato",
+        "answer": "Colegio de la Santa Cruz de Tlatelolco"
+    },
+    {
+        "question": "Ciudad que floreció en el Valle de México tras el abandono de Cuicuilco",
+        "answer": "Teotihuacán"
+    },
+    {
+        "question": "La división básica de la sociedad Nahua era",
+        "answer": "Pipiltin/Maeehual"
+    },
+    {
+        "question": "Con la llegada al trono español de Felipe V, la Nueva España fue sometida a una serie de procesos de modernización. ¿Cómo se le conoce a esta etapa?",
+        "answer": "Las Reformas Borbónicas"
+    },
+    {
+        "question": "Precepto no incluido en el Plan de Iguala de febrero de 1821",
+        "answer": "La Creación de una República"
+    },
+    {
+        "question": "¿Cuál fue el primer Estado europeo que otorgó su reconocimiento a la existencia de México como país independiente?",
+        "answer": "Gran Bretaña"
+    },
+    {
+        "question": "Año en que Texas proclamó su independencia",
+        "answer": "1836"
+    },
+    {
+        "question": "Levantamiento que dio el triunfo a Porfirio Díaz en 1876",
+        "answer": "Plan de Tuxtepec"
+    },
+    {
+        "question": "Grupos rivales enfrentados a partir de la Convención de Aguascalientes:",
+        "answer": "Convencionistas/Constitucionalistas"
+    },
+    {
+        "question": "Fundaron el periódico opositor Regeneración, cuestionaron la conveniencia de la inversión extranjera y, después de haber sido reprimidos por el gobierno porfirista, redactaron el Programa del Partido Liberal",
+        "answer": "Jesús y Ricardo Flores Magón"
+    },
+    {
+        "question": "¿Durante el gobierno de qué presidente se fundó la Secretaría de Educación Pública?",
+        "answer": "Álvaro Obregón"
+    },
+    {
+        "question": "Presidente que tomó como lema la \"Apertura Democrática\" con el fin de legitimar su gobierno",
+        "answer": "Luis Echeverría"
+    },
+    {
+        "question": "Principales candidatos a la presidencia de México en el año 2000",
+        "answer": "Vicente Fox / Francisco Labastida / Cuauhtémoc Cárdenas"
+    },
+    {
+        "question": "Movimiento cultural que criticó el positivismo y enfatizaba la recuperación de los clásicos; uno de sus mayores exponentes fue Alfonso Reyes",
+        "answer": "Ateneo de la Juventud"
+    },
+    {
+        "question": "Durante el periodo conocido como \"nacionalismo cultural\" qué compositor representó musicalmente el épico pasado mexicano",
+        "answer": "Carlos Chávez"
+    },
+    {
+        "question": "Con tan solo dos obras publicadas, El llano en llamas y Pedro Páramo, se coloca como el escritor mexicano que inicia el alejamiento definitivo del \"nacionalismo cultural\"",
+        "answer": "Juan Rulfo"
+    },
+    {
+        "question": "Pintor que inició lo que se conoce como \"La escuela oaxaqueña\" y cuya obra simboliza la ruptura y la continuidad de la tradición",
+        "answer": "Francisco Toledo"
+    },
+    {
+        "question": "Obra arquitectónica de Mario Pani, designada por la UNESCO como Patrimonio Mundial",
+        "answer": "Campus central de Ciudad Universitaria (UNAM)"
+    },
+    {
+        "question": "¿Qué tipo de clima goza la mayor parte del territorio nacional?",
+        "answer": "Clima seco"
+    },
+    {
+        "question": "Durante el siglo XX, ¿en qué periodo se observó un mayor crecimiento de la población?",
+        "answer": "1960-1970"
+    },
+    {
+        "question": "El segundo país de destino de las exportaciones de México es",
+        "answer": "Canadá"
+    },
+    {
+        "question": "En el presente la mayor parte de la población de México está ocupada en el sector económico",
+        "answer": "Servicios"
+    },
+    {
+        "question": "De los países seleccionados por eIINEGI, ¿en qué lugar se encuentra México respecto al índice de Desarrollo Humano (IDH)?",
+        "answer": "71"
+    },
+    {
+        "question": "Entidades federativas que componen la frontera sur de México",
+        "answer": "Quintana Roo. Chiapas. Campeche y Tabasco"
+    },
+    {
+        "question": "Durante la Guerra entre México y Estados Unidos (1846-1848), ¿quién fue el jefe de las fuerzas expedicionarias para invadir México desde Veracruz?",
+        "answer": "Winfield Scott"
+    },
+    {
+        "question": "Tratado por el cual, entre otras cosas, México cedía a perpetuidad a los Estados Unidos el paso a lo largo de dos rutas (Nogales-Guaymas/Camargo-Mazatlán) que corrían desde la frontera norte al Golfo de California:",
+        "answer": "Tratado McLane-Ocampo"
+    },
+    {
+        "question": " Período histórico en el que la Secretaría de Relaciones Exteriores inició por primera vez una investigación sobre las condiciones de los mexicanos en Estados Unidos",
+        "answer": "El Profiriato"
+    },
+    {
+        "question": "Artículo constitucional que generó grandes controversias entre los inversionistas extranjeros tras la promulgación de la Carta Magna de 1917",
+        "answer": "Artículo 27° constitucional"
+    },
+    {
+        "question": "Como representante de México ante la Sociedad de las Naciones, Isidro Fabela fue el único miembro que en 1938 protestó ante",
+        "answer": "La anexión alemana de Austria"
+    },
+    {
+        "question": " En mayo de 1942, siendo presidente Manuel Ávila Camacho, México declaró lo siguiente en el marco de la Segunda Guerra Mundial",
+        "answer": "El estado de guerra con las potencias del \"Eje\""
+    },
+    {
+        "question": "Durante la Guerra Fría el estatus de México en el Movimiento de los Países No Alineados fue de",
+        "answer": "Observador"
+    },
+    {
+        "question": "Ante la inestabilidad de la zona de Centroamérica y el Caribe, México y Venezuela diseñaron en 1980 un esquema de cooperación basado en el suministro de petróleo en condiciones preferencial es al cual se le conoció como",
+        "answer": "Acuerdo de San José"
+    },
+    {
+        "question": " En 1988, los países miembros del Grupo Contadora se unieron a los Países de Apoyo a Contadora para conformar un Mecanismo Permanente de Consulta y Concertación Política llamado",
+        "answer": "Grupo de Río"
+    },
+    {
+        "question": "La \"cláusula democrática\" fue un tema controversial en las negociaciones de México con:",
+        "answer": "La Unión Europea en el marco de la firma del Acuerdo Global"
+    },
+    {
+        "question": "¿En qué periodos México ha formado parte del Consejo de Seguridad de la ONU, como miembro no permanente?",
+        "answer": "1946, 1980-1981, 2002-2003 Y 2009-2010"
+    },
+    {
+        "question": "En cuanto a la problemática de la existencia de \"dos Chinas\", la postura de México a partir de 1972 ha sido",
+        "answer": "Reconocimiento de la República Popular China"
+    },
+    {
+        "question": "En Estados Unidos la \"acción ejecutiva\" conocida como Deferred Action for Childhood Arriva/s (DACA) otorga los siguientes beneficios",
+        "answer": "protege de la deportación por tres años a personas indocumentadas y les permite obtener un permiso para trabajar legalmente"
+    },
+    {
+        "question": "Ante el golpe de Estado que destituyó al presidente Manuel Zelaya de Honduras en 2009, México:",
+        "answer": "Retiró a su embajador, condenó el hecho y no reconoció al gobierno interino de Roberto Micheletti"
+    },
+    {
+        "question": "Los siguientes países conforman el grupo conocido como MIKTA",
+        "answer": "México, Indonesia, Corea del Sur, Turquía, Australia"
+    },
+    {
+        "question": "Dentro de las responsabilidades del Instituto de los Mexicanos en el Exterior (IME) se encuentra",
+        "answer": "Promover la integración de los migrantes a su país de residencia por medio de programas de salud, educación y desarrollo de redes de liderazgos"
+    },
+    {
+        "question": "País que es el principal socio comercial de México entre los países árabes y que hoy en día se identifica como prioritario para profundizar la relación bilateral y llegar a una sociedad estratégica",
+        "answer": "Arabia Saudita"
+    },
+    {
+        "question": "En el debate sobre la reforma del Consejo de Seguridad de la ONU, México se encuentra dentro del grupo \"Unidos por el Consenso\" en compañía de",
+        "answer": "Colombia, Italia, España y Canadá"
+    },
+    {
+        "question": "En 2005 México firmó el primer tratado de libre comercio con un país asiático, ¿cuál fue este país? ",
+        "answer": "Japón"
+    },
+    {
+        "question": "Los sectores productivos mexicanos que muestran un desempeño sobresaliente en las cadenas globales de valor de América del Norte son",
+        "answer": "Automotor, autopartes, aeronáutico y electrónico"
+    },
+    {
+        "question": "Ante los escasos avances producidos en la Conferencia de Desarme de la ONU, México ha liderado en años recientes -junto con Noruega y Austria- un proceso multilateral paralelo dirigido a impulsar la prohibición y eliminación de las armas nucleares conocido como",
+        "answer": "Iniciativa Humanitaria"
+    },
+    {
+        "question": " En el año 2008 el Plan Puebla-Panamá experimentó una restructuración y dio paso al actual mecanismo de cooperación llamado",
+        "answer": "Proyecto de Integración y Desarrollo de Mesoamérica"
+    },
+    {
+        "question": "Dentro del universo de los diferentes procesos de integración latinoamericana, la Alianza del Pacífico responde a la lógica del",
+        "answer": "Regionalismo abierto"
+    },
+    {
+        "question": " Entre los resultados de la décimo sexta Conferencia de las Partes de la Convención Marco de Naciones Unidas sobre el Cambio Climático (COP16) celebrada en Cancún se encontró la siguiente iniciativa apoyada por México",
+        "answer": "El establecimiento del Fondo Verde para el Clima"
+    },
+    {
+        "question": "Año en que por vez primera el PRI perdió la mayoría absoluta en la Cámara de Diputados",
+        "answer": "1997"
+    }
+    /*, 50
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        },
+        {
+            "question": "",
+            "answer": ""
+        }*/
 ];
 
 var backgrounds = [
